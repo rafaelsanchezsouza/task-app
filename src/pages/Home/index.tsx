e53b80f0262e52cc5139d139f1f23501f3565089
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-08-23 09:37:27 -0300

    feat: implement client side task state change

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StaticIsLoading } from '../../components/StaticIsLoading';
-import { useFetch } from '../../customHooks/useFetch';
 import api from '../../services/api';
 
 import styles from './styles.module.scss';
@@ -17,9 +16,9 @@ async function onCreateTask(task: string) {
   }
 }
 
-async function handleDelete(taskId: number) {
+async function handleDelete(task: Task) {
   try {
-    await api.delete(`/tasks/${taskId}`);
+    await api.delete(`/tasks/${task.id}`);
     window.location.reload();
   } catch (err) {
     console.log(err);
@@ -27,10 +26,11 @@ async function handleDelete(taskId: number) {
   }
 }
 
-async function handleChange(taskId: number) {
+async function handleChange(task: Task) {
   try {
-    await api.put(`/tasks/${taskId}`);
-    window.location.reload();
+    await api.put(`/tasks/${task.id}`);
+    return !task.done;
+    // window.location.reload();
   } catch (err) {
     console.log(err);
     alert(err.response.data.message);
@@ -38,9 +38,25 @@ async function handleChange(taskId: number) {
 }
 
 export function Home() {
-  const { data, error, isLoading } = useFetch('/tasks');
-
   const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([] as Task[]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api
+      .get('/tasks')
+      .then(async (response) => {
+        const json = await response.data.items;
+        setTasks(json);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     <StaticIsLoading />;
@@ -50,67 +66,87 @@ export function Home() {
     return <h1>Deu algum erro...</h1>;
   }
 
-  return (
-    <div className={styles.main}>
-      <div className={styles.container}>
-        <form className={styles.createTaskForm}>
-          <input
-            value={task}
-            placeholder={'Task'}
-            onChange={(event) => setTask(event.target.value)}
-          />
-          <br></br>
+  if (true) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <form className={styles.createTaskForm}>
+            <input
+              value={task}
+              placeholder={'Task'}
+              onChange={(event) => setTask(event.target.value)}
+            />
+            <br></br>
 
-          <div className={styles.buttons}>
-            <button
-              type="button"
-              onClick={() => {
-                onCreateTask(task);
-              }}
-              className={styles.submitButton}
-            >
-              Create Task
-            </button>
-          </div>
-        </form>
-        <table className={styles.tasksList}>
-          <tbody>
-            {data.map((task: Task) => {
-              return (
-                <tr key={task.id} className={styles.items}>
-                  <td className={styles.checkboxContainer}>
-                    <input
-                      className={styles.checkbox}
-                      type="checkbox"
-                      id={`done-${task.id}`}
-                      checked={task.done}
-                      onChange={() => {
-                        handleChange(task.id);
-                      }}
-                    />
-                    <span
-                      className={styles.checkmark}
-                      onClick={() => {
-                        handleChange(task.id);
-                      }}
-                    ></span>
-                  </td>
-                  <td>{task.item}</td>
-                  <td>
-                    <button
-                      className={styles.trashButton}
-                      type="button"
-                      onClick={() => handleDelete(task.id)}
-                    >
-                      <img src="/trash.svg" alt="Excluir Empresa" />
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+            <div className={styles.buttons}>
+              <button
+                type="button"
+                onClick={() => {
+                  onCreateTask(task);
+                }}
+                className={styles.submitButton}
+              >
+                Create Task
+              </button>
+            </div>
+          </form>
+          <table className={styles.tasksList}>
+            <tbody>
+              {tasks.map((task: Task) => {
+                return (
+                  <tr key={task.id} className={styles.items}>
+                    <td className={styles.checkboxContainer}>
+                      <input
+                        className={styles.checkbox}
+                        type="checkbox"
+                        id={`done-${task.id}`}
+                        checked={task.done}
+                        onChange={() => {
+                          setTasks(
+                            tasks.map((taskState: Task) => {
+                              if (task.id === taskState.id) {
+                                taskState.done = !taskState.done;
+                              }
+                              return taskState;
+                            })
+                          );
+                          api.put(`/tasks/${task.id}`);
+                        }}
+                      />
+                      <span
+                        className={styles.checkmark}
+                        onClick={() => {
+                          setTasks(
+                            tasks.map((taskState: Task) => {
+                              if (task.id === taskState.id) {
+                                taskState.done = !taskState.done;
+                              }
+                              return taskState;
+                            })
+                          );
+                          api.put(`/tasks/${task.id}`);
+                        }}
+                      ></span>
+                    </td>
+                    <td>{task.item}</td>
+                    <td>
+                      <button
+                        className={styles.trashButton}
+                        type="button"
+                        onClick={() => handleDelete(task)}
+                      >
+                        <img src="/trash.svg" alt="Excluir Empresa" />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <StaticIsLoading />;
+  }
 }
